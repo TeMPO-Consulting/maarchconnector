987404044c801505c4e7513571db5f261412e2fa
Julie Nuguet <julie@nuguet.fr>
2015-11-24 16:55:07 +0100

    checking of the server address format

--- models/configuration.py
+++ models/configuration.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api, exceptions
+import re
 
 class Configuration(models.Model):
 
@@ -13,22 +14,22 @@ class Configuration(models.Model):
     maarch_user_login = fields.Char(string=u"Identifiant de l'utilisateur Maarch", required=True)
     maarch_user_password = fields.Char(string=u"Mot de passe", required=True)
 
-    # verification constraint (SQL)
+    # check that the name is unique
     _sql_constraints = [
         ('name_unique',
          'UNIQUE(name)',
          "Le nom de la configuration doit être unique."),
     ]
 
-    # verification constraint (Python)
-    # TODO
-    """
-    @api.constrains('name', 'server_address', 'maarch_user_id', 'maarch_user_password')
-    def _check_instructor_not_in_attendees(self):
+    @api.constrains('server_address')
+    def _server_address_format_validation(self):
+        """
+        Add "http://" to the server address if it hasn't been mentionned
+        :return:
+        """
         for r in self:
-            if ....:
-                raise exceptions.ValidationError("Format invalide")
-    """
+            if not re.search('^https?://', r.server_address):
+                r.server_address = 'http://%s' % r.server_address
 
 
 
