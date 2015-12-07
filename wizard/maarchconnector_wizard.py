7cd508c3d23b5145c964e1c66e5a237c933bd179
Julie Nuguet <julie@nuguet.fr>
2015-12-07 16:44:51 +0100

    wizard draft

--- wizard/maarchconnector_wizard.py
+++ wizard/maarchconnector_wizard.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from suds.client import Client
 
 
 class Wizard(models.TransientModel):
@@ -10,6 +11,27 @@ class Wizard(models.TransientModel):
 
     @api.multi
     def search_files(self):
-        # TODO
-        pass
+        # TODO : replace the test data
+        _client_maarch = Client("http://10.0.0.195/maarch15/ws_server.php?WSDL", username="bblier", password="maarch")
+
+        param = _client_maarch.factory.create('customizedSearchParams')
+        param.subject = self.filesubject
+        response = _client_maarch.service.customizedSearchResources(param)
+
+        if response:
+            docslist = response[0]
+            with open('/tmp/testlog.txt', 'a') as f:
+                for doc in docslist:
+                    f.write("filesubject : %s\n" % doc.subject.encode('utf8'))
+
+        return {
+            'name': 'Recherche d\'un document dans Maarch',
+            'type': 'ir.actions.act_window',
+            'res_model': 'maarchconnector.wizard',
+            'view_mode': 'form',
+            'view_type': 'form',
+            # 'res_id': this.id,
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
 
