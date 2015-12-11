627f1a241e2995d18aada40195699b0c945c5ac8
Julie Nuguet <julie@nuguet.fr>
2015-12-11 09:49:19 +0100

    refactoring in progress

--- controllers/main.py
+++ controllers/main.py
@@ -15,9 +15,7 @@ import os
 
 class MyBinary(Binary):
 
-    _url_maarch = ''
-    _user_maarch = ''
-    _password_maarch = ''
+    _client_maarch = None
     _filesubject_in_maarch = ''
 
     @http.route()
@@ -34,7 +32,7 @@ class MyBinary(Binary):
                     var win = window.top.window;
                     win.jQuery(win).trigger(%s, %s);
                 </script>"""
-        if self.get_the_active_conf().get('is_conf_active'):
+        if self.is_conf_active().get('is_conf_active'):
             try:
                 if not self._filesubject_in_maarch:
                     # if the user hasn't mentionned any subject we use the filename
@@ -56,19 +54,15 @@ class MyBinary(Binary):
         return super(MyBinary, self).upload_attachment(callback, model, id, ufile)
 
     @http.route('/tempo/maarchconnector/get_the_active_conf', type='json', auth='user')
-    def get_the_active_conf(self):
+    def is_conf_active(self):
         """
-        Indicate if a Maarch configuration is active. If so, get the corresponding datas.
+        Indicate if a Maarch configuration is active.
         """
+        ret = False
         configuration_model = request.registry["maarchconnector.configuration"]
-        active_conf = configuration_model.get_the_active_configuration(request.cr, request.uid, [])
-        is_conf_active = False
-        if active_conf:
-            self._url_maarch = '%s/ws_server.php?WSDL' % active_conf.server_address
-            self._user_maarch = active_conf.maarch_user_login
-            self._password_maarch = active_conf.maarch_user_password
-            is_conf_active = True
-        return {'is_conf_active': is_conf_active}
+        if configuration_model.get_the_active_configuration(request.cr, request.uid, []):
+            ret = True
+        return {'is_conf_active': ret}
 
     @http.route('/tempo/maarchconnector/set_subject', type='json', auth='none')
     def set_subject(self, subject):
@@ -79,6 +73,32 @@ class MyBinary(Binary):
         """
         self._filesubject_in_maarch = subject
 
+    @http.route('/tempo/maarchconnector/client_creation', type='json', auth='user')
+    def client_creation(self, call_from_js=False):
+        self._client_maarch = None
+        error = ''
+        configuration_model = request.registry["maarchconnector.configuration"]
+        active_conf = configuration_model.get_the_active_configuration(request.cr, request.uid, [])
+        if active_conf:
+            url_maarch = '%s/ws_server.php?WSDL' % active_conf.server_address
+            user_maarch = active_conf.maarch_user_login
+            password_maarch = active_conf.maarch_user_password
+            try:
+                self._client_maarch = Client(url_maarch, username=user_maarch, password=password_maarch)
+            except urllib2.URLError:
+                error = "accès au serveur impossible.<br/>L'adresse fournie est incorrecte, " \
+                        "ou le serveur est indisponible."
+            except suds.transport.TransportError:
+                error = "connexion impossible.<br/>Vérifiez l'URL et les identifiants de connexion fournis."
+            except Exception as e:
+                error = e.message
+            if error:
+                error = "Une erreur s'est produite lors du traitement avec Maarch&nbsp: %s" % error
+                if call_from_js:
+                    return {'error': error}  # if the method is called from the JS part
+                else:
+                    raise exceptions.ValidationError(error)  # if the method is called from the Python part
+
     def _add_to_maarch(self, base64_encoded_content, document_subject, extension='pdf'):
         """
         Add the file into Maarch under the name "document_subject"
@@ -86,43 +106,33 @@ class MyBinary(Binary):
         :param document_subject: file name or subject
         :return:
         """
-        try:
-            _client_maarch = Client(self._url_maarch, username=self._user_maarch, password=self._password_maarch)
-            error = ''
-            mydate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            # data relative to the document
-            data = _client_maarch.factory.create('arrayOfData')
-            typist = _client_maarch.factory.create('arrayOfDataContent')
-            typist.column = 'typist'
-            typist.value = 'odoo'
-            typist.type = 'string'
-            doc_date = _client_maarch.factory.create('arrayOfDataContent')
-            doc_date.column = 'doc_date'
-            doc_date.value = mydate
-            doc_date.type = 'string'
-            type_id = _client_maarch.factory.create('arrayOfDataContent')
-            type_id.column = 'type_id'
-            type_id.value = '15'  # misc. by default
-            type_id.type = 'string'
-            subject = _client_maarch.factory.create('arrayOfDataContent')
-            subject.column = 'subject'
-            subject.value = document_subject.decode('utf8')
-            subject.type = 'string'
-            data.datas.append(typist)
-            data.datas.append(doc_date)
-            data.datas.append(type_id)
-            data.datas.append(subject)
-            if not extension:
-                extension = 'pdf'
-            # call to the web service method
-            _client_maarch.service.storeResource(base64_encoded_content, data, 'letterbox_coll',
-                                                 'res_letterbox', extension, 'INIT')
-        except urllib2.URLError:
-            error = "accès au serveur impossible.<br/>L'adresse fournie est incorrecte, ou le serveur est indisponible."
-        except suds.transport.TransportError:
-            error = "connexion impossible.<br/>Vérifiez l'URL et les identifiants de connexion fournis."
-        except Exception:
-            error = "une erreur est survenue lors du traitement."
-        if error:
-            raise exceptions.ValidationError("La pièce jointe ne peut pas être enregistrée dans Maarch&nbsp: %s" % error)
+        self.client_creation()
+        mydate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # data relative to the document
+        data = self._client_maarch.factory.create('arrayOfData')
+        typist = self._client_maarch.factory.create('arrayOfDataContent')
+        typist.column = 'typist'
+        typist.value = 'odoo'
+        typist.type = 'string'
+        doc_date = self._client_maarch.factory.create('arrayOfDataContent')
+        doc_date.column = 'doc_date'
+        doc_date.value = mydate
+        doc_date.type = 'string'
+        type_id = self._client_maarch.factory.create('arrayOfDataContent')
+        type_id.column = 'type_id'
+        type_id.value = '15'  # misc. by default
+        type_id.type = 'string'
+        subject = self._client_maarch.factory.create('arrayOfDataContent')
+        subject.column = 'subject'
+        subject.value = document_subject.decode('utf8')
+        subject.type = 'string'
+        data.datas.append(typist)
+        data.datas.append(doc_date)
+        data.datas.append(type_id)
+        data.datas.append(subject)
+        if not extension:
+            extension = 'pdf'
+        # call to the web service method
+        self._client_maarch.service.storeResource(base64_encoded_content, data, 'letterbox_coll',
+                                                  'res_letterbox', extension, 'INIT')
 
--- static/src/js/maarchconnector.js
+++ static/src/js/maarchconnector.js
@@ -53,29 +53,43 @@
         on_maarch_doc: function() {
             // method called on click on "Add from Maarch..."
             var self = this;
-            var view = self.getParent();
-            var ids = ( view.fields_view.type != "form" )? view.groups.get_selection().ids : [ view.datarecord.id ];
-            var context = {
-                'model': view.dataset.model,
-                'ids': ids,
-            };
 
-            // used to send data to the "do_action" method
-            var action = {
-                name: "Recherche d'un document dans Maarch",
-                type: 'ir.actions.act_window',
-                res_model: 'maarch.wizard',
-                view_mode: 'form',
-                view_type: 'form',
-                views: [[false, 'form']],
-                target: 'new',
-                context: context,
-            };
-            // used to open the new view
-            self.do_action(action, {
-                // the list of attachments is refreshed when the wizard is closed
-                on_close: function () {
-                    self.do_attachement_update(self.dataset, self.model_id);
+            instance.session.rpc('/tempo/maarchconnector/client_creation', {
+                call_from_js : true
+            }).done(function (result) {
+                if(result && result.error && result.error.length > 0)
+                {
+                    // if the creation of the Maarch client has failed: display an error message
+                    self.do_notify('Connecteur Maarch', result.error, true);
+                }
+                else
+                {
+                    // if the connection with Maarch is OK: display a wizard to search a document to add
+                    var view = self.getParent();
+                    var ids = ( view.fields_view.type != "form" )? view.groups.get_selection().ids : [ view.datarecord.id ];
+                    var context = {
+                        'model': view.dataset.model,
+                        'ids': ids,
+                    };
+
+                    // used to send data to the "do_action" method
+                    var action = {
+                        name: "Recherche d'un document dans Maarch",
+                        type: 'ir.actions.act_window',
+                        res_model: 'maarch.wizard',
+                        view_mode: 'form',
+                        view_type: 'form',
+                        views: [[false, 'form']],
+                        target: 'new',
+                        context: context,
+                    };
+                    // used to open the new view
+                    self.do_action(action, {
+                        // the list of attachments is refreshed when the wizard is closed
+                        on_close: function () {
+                            self.do_attachement_update(self.dataset, self.model_id);
+                        }
+                    });
                 }
             });
         }
