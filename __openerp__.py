6031c42d63f7dc06b7aa56c348b995730d01f78f
Julie Nuguet <julie@nuguet.fr>
2015-12-17 17:10:53 +0100

    read-only access for all

--- __openerp__.py
+++ __openerp__.py
@@ -5,6 +5,7 @@
         'views/maarchconnector.xml',
         'views/maarchconnector_assets.xml',
         'wizard/maarchconnector_wizard_view.xml',
+        'security/ir.model.access.csv',
     ],
     'js': [
         'static/src/js/maarchconnector.js'
